61ad52d74a94627a6346b142275a3ccd760aa424
rudrodip <official.rudrodipsarker@gmail.com>
2024-01-31 23:45:31 +0600

    Add marketing page component

---
+++ src/app/(marketing)/page.tsx
@@ -0,0 +1,13 @@
+import { siteConfig } from "@/config/site";
+export default function Home() {
+  return (
+    <main className="min-h-screen">
+      <h1 className="head-text-md max-w-7xl primary-gradient text-center mt-8 md:mt-16 lg:mt-24">
+        {siteConfig.name}
+      </h1>
+      <p className="text-2xl my-4 text-center max-w-2xl mx-auto">
+        {siteConfig.description}
+      </p>
+    </main>
+  );
+}
