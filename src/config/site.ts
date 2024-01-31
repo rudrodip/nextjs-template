25705ba85efa5ce342e3f8613a79f73e8b4be45c
rudrodip <official.rudrodipsarker@gmail.com>
2024-01-31 23:48:10 +0600

    Add site configuration file

---
+++ src/config/site.ts
@@ -0,0 +1,25 @@
+import { SiteConfig } from "@/types";
+
+export const siteConfig: SiteConfig = {
+  name: "NextJS + Shadcn + Prisma + NextAuth Starter",
+  description:
+    "A starter template for Next.js with Shadcn, Prisma, NextAuth, Tailwind CSS, and TypeScript",
+  url: "https://nextjstemplate.vercel.app",
+  ogImage: "https://nextjstemplate.vercel.app/og.png",
+  creator: {
+    name: "RDS",
+    url: "https://twitter.com/rds_agi",
+  },
+  keywords: [
+    "Next.js",
+    "Shadcn",
+    "Prisma",
+    "NextAuth",
+    "Tailwind CSS",
+    "TypeScript",
+  ],
+  links: {
+    twitter: "https://twitter.com/rds_agi",
+    github: "https://github.com/rudrodip/nextjs-template",
+  },
+};
